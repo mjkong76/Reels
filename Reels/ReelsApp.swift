81f11c885eb24a6a06a9a1df8d238e5236ddf2ac
mjkong76 <mjkong76@gmail.com>
2021-10-31 00:51:56 +0900

    first commit

---
+++ Reels/ReelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelsApp.swift
+//  Reels
+//
+//  Created by 공현택 on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct ReelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
